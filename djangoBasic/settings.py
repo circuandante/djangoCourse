6d687c137a5e8cf960af34a8ca90f9f73d304591
Circuandante <circuandante@gmail.com>
2020-03-05 11:23:32 -0500

    ultimo commit antes de extender modelo de usuario

--- djangoBasic/settings.py
+++ djangoBasic/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangoBasic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_course',
+        'USER': 'postgres',
+        'PASSWORD': 'Andr3$R1v3r4011',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -3,5 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Users(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    """user models"""
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
