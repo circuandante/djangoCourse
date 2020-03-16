ac4584fa99fca0f7e0cdd76beeafc5b694aa8f52
Circuandante <circuandante@gmail.com>
2020-03-16 08:14:31 -0500

    retomando

--- users/models.py
+++ users/models.py
@@ -1,20 +1,10 @@
+"""extendiendo el modelo de usuario"""
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
 class Users(models.Model):
-    """user models"""
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    client = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.email
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=200, blank=True)
